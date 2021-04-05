11766b1c1c224fe750ccdc86ebd711329be25d30
Agehadom <upwithice@yahoo.com>
2021-04-05 19:30:29 -0400

    Fixed vue config.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/domvuefoliojs/'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/domvuefoliojs/'
+  : '/',
   transpileDependencies: [
     'vuetify'
   ]
